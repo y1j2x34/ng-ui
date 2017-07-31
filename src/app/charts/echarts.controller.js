c779a7db438f66ec7069cb44066443870d222f80
y1j2x34 <y1j2x34@qq.com>
2017-07-31 12:57:55 +0800

    删除没用的日志

--- src/app/charts/echarts.controller.js
+++ src/app/charts/echarts.controller.js
@@ -24,15 +24,13 @@ define([
             var instance = Echarts.init($element.get(0));
             self.echartsInstance = instance;
             updateOptions();
-        }
+        };
 
         function updateOptions() {
             $window.clearTimeout(self.updateOptionTimmerId);
-            var stack = new Error().stack;
             self.updateOptionTimmerId = $window.setTimeout(function () {
-                self.echartsInstance["__flagInMainProcess"] = false;
+                self.echartsInstance.__flagInMainProcess = false;
                 self.echartsInstance.setOption(self.options);
-                console.info("更新了一次", stack);
             }, 40);
         }
     }
